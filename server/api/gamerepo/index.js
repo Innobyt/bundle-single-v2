97bc2134fa50cafbb10e8a56cd30a0fecc2e61aa
Calvyn Tng <calvyn.twh@gmail.com>
2015-01-05 06:52:44 +0800

    Minor changes
    added CORS code for cross domain response,
    removes register button in login page.

--- server/api/gamerepo/index.js
+++ server/api/gamerepo/index.js
@@ -5,6 +5,18 @@ var controller = require('./gamerepo.controller');
 
 var router = express.Router();
 
+//testcode for cors cross domain response *calvyn
+
+router.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+//	res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Authorization, Accept');
+	next();
+});
+
+router.options('*', function(req, res) {
+    res.send(200);
+});
 // crud
 router.post('/', controller.create);
 router.get('/', controller.index);
